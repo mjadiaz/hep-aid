ae636671df3d2ee6a223e8589740f50eb3e453e4
mjadiaz <jmauricio.adiaz@gmail.com>
2021-08-27 15:10:38 +0100

    del readme

--- README.m
+++
@@ -1,8 +0,0 @@
-# HepAid
-
-### [HepRead](HepRead.py): 
-Python classes to read, edit and create LesHouches files to easily create scans 
-with SPheno. 
-
-### [HepTools](HepTools.py):
-Python classes and methods to run HEP tools from python scripts.

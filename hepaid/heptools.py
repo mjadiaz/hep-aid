d97a210952ee2eca7b4350576702c92b7ddd9c3b
mjadiaz <jmauricio.adiaz@gmail.com>
2021-10-04 13:08:12 +0100

    update setup

--- hepaid/heptools.py
+++ hepaid/heptools.py
@@ -7,9 +7,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-import HepRead
-
-
 
 class Spheno:
     '''
--- setup.py
+++ setup.py
@@ -1,2 +1,2 @@
 import setuptools
-setuptools.setup(packages=['HepTools', 'HepRead'])
+setuptools.setup()
